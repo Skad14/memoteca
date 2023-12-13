b2c5b19a8816fb66f427c8e545bd66233d866ccd
Rafael Franca <rafa-cross@hotmail.com>
2023-12-13 17:42:45 -0300

    interface de controle de dados do backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut malesuada tortor. Nulla semper rutrum nisi, a imperdiet lorem vulputate et. Quisque tempor nisl mollis orci auctor, vel vestibulum erat aliquam. Aenean rhoncus leo vitae est fringilla tincidunt. Aenean hendrerit, justo quis mollis maximus, tellus dui maximus lacus, a cursus sem leo quis mauris. Maecenas cursus volutpat dui, vitae tempus mi lacinia in. Vivamus rhoncus auctor lorem, at ornare dui mattis vel. Nulla vel tellus neque.',
-      autoria: 'Lorem Ipsum',
-      modelo: 'modelo1'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
