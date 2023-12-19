3278a3f1766c225dd35440cdd3a08a69e143da3c
Rafael Franca <rafa-cross@hotmail.com>
2023-12-19 17:03:43 -0300

    formulário reativo utilizando FormBuilder

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -10,7 +10,9 @@ export class PensamentoService {
 
   private readonly API = 'http://localhost:3000/pensamentos'
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   listar(): Observable<Pensamento[]> {
     return this.http.get<Pensamento[]>(this.API)
